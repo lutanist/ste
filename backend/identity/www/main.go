d35d5ac792cf6c24fa1ecadb9f00685df0fbd0f9
김한석 <han@spidor.co.kr>
2022-12-12 21:42:51 +0900

    tt

--- backend/identity/www/main.go
+++ backend/identity/www/main.go
@@ -8,9 +8,30 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/gofiber/template/html"
+	"github.com/google/wire"
 
+	"github.com/lutanist/ste/backend/identity/core"
 	"github.com/lutanist/ste/backend/identity/handlers"
 )
+
+func newApp(h *handlers.Handler) *fiber.App {
+	engine := html.NewFileSystem(http.Dir("./views"), ".html")
+	// Reload the templates on each render, good for development
+	engine.Reload(true)
+	// Debug will print each template that is parsed, good for debugging
+	engine.Debug(true)
+
+	app := fiber.New(fiber.Config{
+		Views:   engine,
+		Prefork: false,
+	})
+
+	app.Use(logger.New())
+	app.Use(recover.New())
+
+	h.Register(app)
+	return app
+}
 
 func main() {
 	engine := html.NewFileSystem(http.Dir("./views"), ".html")
@@ -25,6 +46,14 @@ func main() {
 	})
 	app.Use(logger.New())
 	app.Use(recover.New())
+
+	wire.Build(
+		wire.Bind(new(core.AuthenticationScheme), new(*core.DefaultAuthenticationSchemeProvider)),
+		core.NewDefaultAuthenticationSchemeProvider,
+		core.NewSignInManager,
+		handlers.NewHandler,
+		newApp,
+	)
 
 	// TODO: initialize sign in manager
 
